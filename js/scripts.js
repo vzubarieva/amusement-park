8083904d7824b5b7f11daf0e99c465e50e27bcdd
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-04-04 23:37:01 -0700

    Add highlighting based on height

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const height = parseInt(prompt("What is your height in inches?"));
 
-  if ( height >= 42) {
-    $('#rides').show();
+  if (height >= 120) {
+    $(".ride-medium, .ride-tall").addClass("highlight");
+  } else if (height >= 100) {
+    $(".ride-short").addClass("highlight");
   }
 });
+
